52e45a54eafec539ebccb0b3bfff0a2c2f858485
marshal <marshal.wu@gmail.com>
2022-03-03 22:40:12 +0800

    first commit

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import Koa from 'koa';
+import Koa2Cors from 'koa2-cors';
+import KoaBodyParser from 'koa-bodyparser';
+import Router from 'koa-router';
+
+
+const Server = new Koa();
+const router = new Router();
+
+router.get('/hello', async (ctx: Koa.BaseContext, next) => {
+    ctx.body = { message: 'Hello World!' };
+    await next();
+});
+
+Server.use(Koa2Cors()).use(KoaBodyParser());
+Server.use(router.routes()).use(router.allowedMethods());
+
+Server.listen(4000, () => {
+    console.log('Server listening port 4000.');
+});
